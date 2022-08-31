a61b7fe8b82dc72918b058aaaecb0632c9280908
Arahmna75 <selfaraman01825@gmail.com>
2022-08-31 10:31:33 +0600

    added a phone detail

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
+// spinner
+
+
+const toggleSpinner = (displayStyle)=>{
+    document.getElementById('spinner').style.display = displayStyle;
+   
+}
+
+document.getElementById('error-message').style.display = 'none';
 const searchPhone = ()=>{
     const searchText = document.getElementById('search-field').value;
     // console.log(searchText)
+    toggleSpinner('block');
+   
     loadPhone(searchText);
     document.getElementById('search-field').value = '';
+    document.getElementById('error-message').style.display = 'none';
 }
 
 const loadPhone =(searchText) =>{
     const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayLoadPhone(data.data));
+    .then(data => displayLoadPhone(data.data))
+.catch(error => displayError(error));
+    
     
 }
+
+const displayError = (error) =>{
+    
+    document.getElementById('error-message').style.display = 'block';
+}
+
 loadPhone('iphone');
 
 const displayLoadPhone = (phones) =>{
@@ -24,15 +44,15 @@ const displayLoadPhone = (phones) =>{
         div.innerHTML = `
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h3 class="card-title">${phone.brand}</h3>
-          <h5 class="card-title">${phone.phone_name}</h5>
-          <button onclick="loadPhoneDetail('${phone.slug}')">Phone Details</button>
+          <h3 class="card-title text-center">${phone.brand}</h3>
+          <h5 class="card-title text-center">${phone.phone_name}</h5>
+          <p class="text-center"><button class="card-button" onclick="loadPhoneDetail('${phone.slug}')">Phone Details</button></p>
           
         </div>
         `;
         container.appendChild(div);
-    })
-    
+    });
+    toggleSpinner('none');
 }
 
 const loadPhoneDetail = (slug)=>{
@@ -51,10 +71,18 @@ const displayPhoneDetail =(phoneDetail)=>{
     div.innerHTML = `
     <img src="${phoneDetail.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h2 class="card-title">ReleaseDate: ${phoneDetail.releaseDate ? phoneDetail.releaseDate : 'ReleaseDate is not found.'}</h2>
-      <h3 class="card-title">ChipSet: ${phoneDetail.mainFeatures.chipSet}</h3>
-      <h4 class="card-title">Memory: ${phoneDetail.mainFeatures.memory}</h4>
-      <h4 class="card-title">Sensors: ${phoneDetail.mainFeatures.sensors}</h4>
+      <h2 class="card-title text-center">ReleaseDate: ${phoneDetail.releaseDate ? phoneDetail.releaseDate : 'ReleaseDate is not found.'}</h2>
+      <h2 class="card-title text-center">MainFeatures</h2>
+      <h3 class="card-title text-center">ChipSet: ${phoneDetail.mainFeatures.chipSet}</h3>
+      <h4 class="card-title text-center">Memory: ${phoneDetail.mainFeatures.memory}</h4>
+      <h4 class="card-title text-center">Sensors: ${phoneDetail.mainFeatures.sensors}</h4>
+      
+      <h2 class="card-title text-center">Others</h2>
+      <h5 class="card-title text-center">Bluetooth: ${phoneDetail.others.Bluetooth}</h5>
+      <h5 class="card-title text-center">GPS: ${phoneDetail.others.GPS}</h5>
+      <h5 class="card-title text-center">USB: ${phoneDetail.others.USB}</h5>
+      <h5 class="card-title text-center">Radio: ${phoneDetail.others.Radio}</h5>
+      <h5 class="card-title text-center">WLAN: ${phoneDetail.others.WLAN}</h5>
       
     </div>
     `;
